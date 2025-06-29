746e23aa99665d7c5106abd66500dd6239d87ebb
lhutinet <lhutinet@free.fr>
2025-06-29 23:48:52 +0200

    003.06-modif cv

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,67 +2,59 @@ import React from "react";
 import Navigation from "../components/Navigation";
 
 const Contact = () => {
-  return (
-    <div className="contact">
-      <Navigation />
-      <div className="contactContent">
-        <div className="header"></div>
-        <div className="contactBox">
-          <h1>Contactez-moi</h1>
-          <ul>
-            <li>
-              <i className="fas fa-map-marker-alt"></i>
-              <span>bordeaux</span>
-            </li>
-            <li>
-              <i className="fas fa-mobile-alt"></i>
-              <span className="clickInput">06 62 31 68 23</span>
-            </li>
-            <li>
-              <i className="far fa-envelope"> </i>
-              <span className="clickInput">Mail:lhutinet@free.fr</span>
-            </li>
-          </ul>
-        </div>
-        <div className="socialNetwork">
-          <ul>
-            <a
-              href="https://www.google.com/"
-              rel="noopener noreferrer"
-              target="_blank"
-            >
-              <h4>LinkedIn</h4>
-              <i className="fab fa-linkedin"></i>
-            </a>
-            <a
-              href="httpnoopeners://www.google.com/"
-              rel="noopener noreferrer"
-              target="_blank"
-            >
-              <h4>GitHub</h4>
-              <i className="fab fa-github"></i>
-            </a>
-            <a
-              href="https://www.google.com/"
-              rel="noopener noreferrer"
-              target="_blank"
-            >
-              <h4>Twitter</h4>
-              <i className="fab fa-twitter"></i>
-            </a>
-            <a
-              href="https://www.google.com/"
-              rel="noopener noreferrer"
-              target="_blank"
-            >
-              <h4>CodePen</h4>
-              <i className="fab fa-codepen"></i>
-            </a>
-          </ul>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="contact">
+			<Navigation />
+			<div className="contactContent">
+				<div className="header"></div>
+				<div className="contactBox">
+					<h1>Contactez-moi</h1>
+					<ul>
+						<li>
+							<i className="fas fa-map-marker-alt"></i>
+							<span>3 grande rue 38150 Roussillon</span>
+						</li>
+						<li>
+							<i className="fas fa-mobile-alt"></i>
+							<span className="clickInput">06 62 31 68 23</span>
+						</li>
+						<li>
+							<i className="far fa-envelope"> </i>
+							<span className="clickInput">Mail:lhutinet@free.fr</span>
+						</li>
+					</ul>
+				</div>
+				<div className="socialNetwork">
+					<ul>
+						<a
+							href="https://www.linkedin.com/in/laurent-hutinet-8391ba338/"
+							rel="noopener noreferrer"
+							target="_blank"
+						>
+							<h4>LinkedIn</h4>
+							<i className="fab fa-linkedin"></i>
+						</a>
+						<a
+							href="https://github.com/lhutinet"
+							rel="noopener noreferrer"
+							target="_blank"
+						>
+							<h4>GitHub</h4>
+							<i className="fab fa-github"></i>
+						</a>
+						<a
+							href="https://x.com/pludsou1"
+							rel="noopener noreferrer"
+							target="_blank"
+						>
+							<h4>Twitter</h4>
+							<i className="fab fa-twitter"></i>
+						</a>
+					</ul>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Contact;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,22 +2,22 @@ import React from "react";
 import Navigation from "../components/Navigation";
 
 const Home = () => {
-  return (
-    <div className="home">
-      <Navigation />
-      <div className="homeContent">
-        <div className="content">
-          <h1>Laurent Hutinet</h1>
-          <h2>Developpeur Front-end</h2>
-          <div className="pdf">
-            <a href="./media/CV.pdf" target="_blank">
-              Télécharger CV
-            </a>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="home">
+			<Navigation />
+			<div className="homeContent">
+				<div className="content">
+					<h1>Laurent Hutinet</h1>
+					<h2>Developpeur Front-end</h2>
+					<div className="pdf">
+						<a href="./media/2025-02-07-LAURENT HUTINET.pdf" target="_blank">
+							Télécharger CV
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Home;
