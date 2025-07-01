d70e61077895c8b7661c941d1d38ce2e02a53d06
lhutinet <lhutinet@free.fr>
2025-07-01 16:05:16 +0200

    005.00 suppression pdf ajout a la place d'un texte de presentation petit changements

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,9 +10,24 @@ const Home = () => {
 					<h1>Laurent Hutinet</h1>
 					<h2>Developpeur Front-end</h2>
 					<div className="pdf">
-						<a href="./media/2025-02-07-LAURENT HUTINET.pdf" target="_blank">
+						{/* <a href="./media/2025-02-07-LAURENT HUTINET.pdf" target="_blank">
 							Télécharger CV
-						</a>
+						</a> */}
+						<p>
+							Développeur web polyvalent, je crée des sites internet sur
+							WordPress, Wix ou en code personnalisé (HTML, CSS, JavaScript,
+							Vue.js).
+							<br />
+							<br />
+							Je conçois des projets sur mesure, modernes et responsives, en
+							privilégiant la clarté et la qualité du code.
+							<br />
+							<br />
+							Mon objectif :
+							<br />
+							<br />
+							rendre votre présence en ligne unique et efficace.
+						</p>
 					</div>
 				</div>
 			</div>
